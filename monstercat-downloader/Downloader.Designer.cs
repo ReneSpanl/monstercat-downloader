2c72c7e99c6392c1b7c29213d9f969205bcda19b
Rene Spanl <ReneSpanl@users.noreply.github.com>
2017-11-25 23:11:35 +0100

    First and only commit.

---
+++ monstercat-downloader/Downloader.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+
+namespace monstercat_downloader
+{
+    partial class Main_Window
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_get_releases = new System.Windows.Forms.Button();
+            this.btn_start_download = new System.Windows.Forms.Button();
+            this.Box_debug = new System.Windows.Forms.TextBox();
+            this.lbl_rls_count = new System.Windows.Forms.Label();
+            this.btn_debug = new System.Windows.Forms.Button();
+            this.box_cookie = new System.Windows.Forms.TextBox();
+            this.lbl_cookie = new System.Windows.Forms.Label();
+            this.pb_dl = new System.Windows.Forms.ProgressBar();
+            this.lbl_curtitle = new System.Windows.Forms.Label();
+            this.chck_podcast = new System.Windows.Forms.CheckBox();
+            this.chck_mixes = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btn_get_releases
+            // 
+            this.btn_get_releases.Location = new System.Drawing.Point(13, 12);
+            this.btn_get_releases.Name = "btn_get_releases";
+            this.btn_get_releases.Size = new System.Drawing.Size(75, 23);
+            this.btn_get_releases.TabIndex = 0;
+            this.btn_get_releases.Text = "getRealeses";
+            this.btn_get_releases.UseVisualStyleBackColor = true;
+            this.btn_get_releases.Click += new System.EventHandler(this.btn_get_releases_Click);
+            // 
+            // btn_start_download
+            // 
+            this.btn_start_download.Location = new System.Drawing.Point(13, 57);
+            this.btn_start_download.Name = "btn_start_download";
+            this.btn_start_download.Size = new System.Drawing.Size(75, 23);
+            this.btn_start_download.TabIndex = 1;
+            this.btn_start_download.Text = "download";
+            this.btn_start_download.UseVisualStyleBackColor = true;
+            this.btn_start_download.Click += new System.EventHandler(this.btn_start_download_Click);
+            // 
+            // Box_debug
+            // 
+            this.Box_debug.Location = new System.Drawing.Point(215, 218);
+            this.Box_debug.Multiline = true;
+            this.Box_debug.Name = "Box_debug";
+            this.Box_debug.Size = new System.Drawing.Size(268, 38);
+            this.Box_debug.TabIndex = 2;
+            // 
+            // lbl_rls_count
+            // 
+            this.lbl_rls_count.AutoSize = true;
+            this.lbl_rls_count.Location = new System.Drawing.Point(546, 12);
+            this.lbl_rls_count.Name = "lbl_rls_count";
+            this.lbl_rls_count.Size = new System.Drawing.Size(91, 13);
+            this.lbl_rls_count.TabIndex = 3;
+            this.lbl_rls_count.Text = "available releases";
+            // 
+            // btn_debug
+            // 
+            this.btn_debug.Location = new System.Drawing.Point(591, 168);
+            this.btn_debug.Name = "btn_debug";
+            this.btn_debug.Size = new System.Drawing.Size(75, 23);
+            this.btn_debug.TabIndex = 4;
+            this.btn_debug.Text = "Stop Download";
+            this.btn_debug.UseMnemonic = false;
+            this.btn_debug.UseVisualStyleBackColor = true;
+            this.btn_debug.Click += new System.EventHandler(this.btn_debug_Click);
+            // 
+            // box_cookie
+            // 
+            this.box_cookie.Location = new System.Drawing.Point(591, 57);
+            this.box_cookie.Name = "box_cookie";
+            this.box_cookie.Size = new System.Drawing.Size(100, 20);
+            this.box_cookie.TabIndex = 5;
+            this.box_cookie.TextChanged += new System.EventHandler(this.box_cookie_TextChanged);
+            // 
+            // lbl_cookie
+            // 
+            this.lbl_cookie.AutoSize = true;
+            this.lbl_cookie.Location = new System.Drawing.Point(508, 60);
+            this.lbl_cookie.Name = "lbl_cookie";
+            this.lbl_cookie.Size = new System.Drawing.Size(62, 13);
+            this.lbl_cookie.TabIndex = 6;
+            this.lbl_cookie.Text = "connect.sid";
+            // 
+            // pb_dl
+            // 
+            this.pb_dl.ForeColor = System.Drawing.Color.SkyBlue;
+            this.pb_dl.Location = new System.Drawing.Point(36, 163);
+            this.pb_dl.Name = "pb_dl";
+            this.pb_dl.Size = new System.Drawing.Size(521, 31);
+            this.pb_dl.TabIndex = 7;
+            // 
+            // lbl_curtitle
+            // 
+            this.lbl_curtitle.AutoSize = true;
+            this.lbl_curtitle.Location = new System.Drawing.Point(324, 173);
+            this.lbl_curtitle.Name = "lbl_curtitle";
+            this.lbl_curtitle.Size = new System.Drawing.Size(0, 13);
+            this.lbl_curtitle.TabIndex = 8;
+            // 
+            // chck_podcast
+            // 
+            this.chck_podcast.AutoSize = true;
+            this.chck_podcast.Location = new System.Drawing.Point(104, 61);
+            this.chck_podcast.Name = "chck_podcast";
+            this.chck_podcast.Size = new System.Drawing.Size(70, 17);
+            this.chck_podcast.TabIndex = 9;
+            this.chck_podcast.Text = "Podcasts";
+            this.chck_podcast.UseVisualStyleBackColor = true;
+            this.chck_podcast.CheckedChanged += new System.EventHandler(this.chck_podcast_CheckedChanged);
+            // 
+            // chck_mixes
+            // 
+            this.chck_mixes.AutoSize = true;
+            this.chck_mixes.Location = new System.Drawing.Point(181, 61);
+            this.chck_mixes.Name = "chck_mixes";
+            this.chck_mixes.Size = new System.Drawing.Size(53, 17);
+            this.chck_mixes.TabIndex = 10;
+            this.chck_mixes.Text = "Mixes";
+            this.chck_mixes.UseVisualStyleBackColor = true;
+            this.chck_mixes.CheckedChanged += new System.EventHandler(this.chck_mixes_CheckedChanged);
+            // 
+            // Main_Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 270);
+            this.Controls.Add(this.chck_mixes);
+            this.Controls.Add(this.chck_podcast);
+            this.Controls.Add(this.lbl_curtitle);
+            this.Controls.Add(this.pb_dl);
+            this.Controls.Add(this.lbl_cookie);
+            this.Controls.Add(this.box_cookie);
+            this.Controls.Add(this.btn_debug);
+            this.Controls.Add(this.lbl_rls_count);
+            this.Controls.Add(this.Box_debug);
+            this.Controls.Add(this.btn_start_download);
+            this.Controls.Add(this.btn_get_releases);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main_Window";
+            this.Text = "monstercat-downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_get_releases;
+        private System.Windows.Forms.Button btn_start_download;
+        private System.Windows.Forms.TextBox Box_debug;
+        private System.Windows.Forms.Label lbl_rls_count;
+        private System.Windows.Forms.Button btn_debug;
+        private System.Windows.Forms.TextBox box_cookie;
+        private System.Windows.Forms.Label lbl_cookie;
+        private System.Windows.Forms.ProgressBar pb_dl;
+        private System.Windows.Forms.Label lbl_curtitle;
+        private System.Windows.Forms.CheckBox chck_podcast;
+        private System.Windows.Forms.CheckBox chck_mixes;
+    }
+}
+
---
+++ monstercat-downloader/Downloader.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+
+namespace monstercat_downloader
+{
+    public partial class Main_Window : Form
+    {
+        string prgPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        DownloadQueue dq;
+        int releaseCount = 0;
+        int limitNumber = 50;
+        public MCatJSON JDO;
+        WebClient wc = new WebClient();
+        string currentdir;
+        bool con = false;
+        Thread dl_thread;
+        string extension = "flac";
+        bool dl_podcast=false, dl_mixes=false;
+        StreamReader reader;
+        Stream data;
+
+        public Main_Window()
+        {
+            InitializeComponent();
+            writeDebugLine("Startup");
+            dq = new DownloadQueue();
+            currentdir = System.IO.Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath);
+            wc.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+        }
+
+        private void btn_get_releases_Click(object sender, EventArgs e)
+        {
+            labelupdater();
+
+        }
+        private void writeDebugLine(string str)
+        {
+            MethodInvoker methodInvokerDelegate = delegate ()
+            {
+                Box_debug.AppendText(str + "\r\n");
+            };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(methodInvokerDelegate);
+            }
+            else
+            { 
+                methodInvokerDelegate();
+            }
+        }
+        private void update_dl_title(string str)
+        {
+            MethodInvoker methodInvokerDelegate = delegate ()
+            {
+                lbl_curtitle.Text = str;
+            };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(methodInvokerDelegate);
+            }
+            else
+            {
+                methodInvokerDelegate();
+            }
+        }
+        private void pbar_updater(int max, int cur)
+        {
+            MethodInvoker methodInvokerDelegate = delegate ()
+            {
+                pb_dl.Maximum = max;
+                pb_dl.Value = cur;
+            };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(methodInvokerDelegate);
+            }
+            else
+            {
+                methodInvokerDelegate();
+            }
+
+        }
+        private void writeDebugFile(string str)
+        {
+            System.IO.File.AppendAllText(@"log.txt","\n" + str);
+
+        }
+
+        private string returnJSONFromURL(string URL)
+        {
+            writeDebugLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            data = wc.OpenRead(URL);
+            reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+            data = null;
+            reader = null;
+            return s;
+
+        }
+        public int getReleaseCount()
+        {
+            writeDebugLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            MCatJSON J = Newtonsoft.Json.JsonConvert.DeserializeObject<MCatJSON>(returnJSONFromURL("https://connect.monstercat.com/api/catalog/browse/?limit=0")) ;
+            releaseCount = J.total;
+            return J.total;
+            
+
+        }
+        public void labelupdater()
+        {
+            con = true;
+            writeDebugLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            lbl_rls_count.Text = ("available releases: " + getReleaseCount().ToString());
+        }
+
+
+        public string resultToURL(Result r)
+        {
+            return "https://connect.monstercat.com/api/release/" + r.albums.albumId.ToString() + "/download?method=download&type=flac&track=" + r._id.ToString();
+        }
+
+        private void btn_start_download_Click(object sender, EventArgs e)
+        {
+            bool _dl_podcast = dl_podcast;
+            bool _dl_mixes = dl_mixes;
+            if (!con){
+                labelupdater();
+            }
+            if (!Directory.Exists("download"))
+
+            {
+
+                Directory.CreateDirectory("download");
+
+            }
+            if (releaseCount == 0)
+            {
+                MessageBox.Show("could not fetch releaseCount");
+                return;
+            }
+
+            pb_dl.Maximum = releaseCount;
+
+            MCatJSON J;
+            string URLString="";
+            string str;
+            for (int i = 0; i < (releaseCount / limitNumber) + 1; i++)
+            {
+                str = "https://connect.monstercat.com/api/catalog/browse/?limit=" + limitNumber.ToString() + "&skip=" + (i * limitNumber).ToString();
+                writeDebugLine("Fetch String: " + str);
+                J = Newtonsoft.Json.JsonConvert.DeserializeObject<MCatJSON>(returnJSONFromURL(str));
+                for (int x = 0; x < J.results.Count; x++)
+                {
+                    if (J.results[x].streamable == false)
+                    {
+                        writeDebugFile("skipping URL: " + resultToURL(J.results[x]) + " release not yet downloadable");
+
+                    }
+                    else if(_dl_podcast == false && J.results[x].release.type == "Podcast")
+                    {
+                        writeDebugFile("skipping URL: " + resultToURL(J.results[x]) + " release is a Podcast");
+                    }
+                    else {
+                        URLString = resultToURL(J.results[x]);
+                        dq.appendToQueue(new DownloadObject(URLString, J.results[x].artistsTitle  + " - "  + J.results[x].title + " - " + "["+J.results[x].release.catalogId+"]",  J.results[x].artistsTitle, J.results[x].release.title));
+                        pb_dl.Value = i * limitNumber + x;
+                    }
+                }
+
+            }
+            URLString = null;
+            J = null;
+            pb_dl.Value = pb_dl.Maximum;
+            GC.Collect();
+            dl_thread = new Thread(new ThreadStart(queueDownloader));
+            dl_thread.Start();
+        }
+
+        private void btn_debug_Click(object sender, EventArgs e)
+        {
+            if(dl_thread != null)
+            {
+                dl_thread.Abort();
+            }
+                
+        }
+
+
+        public void queueDownloader()
+        {
+            writeDebugLine("fnc: "+System.Reflection.MethodBase.GetCurrentMethod().Name);
+            wc.Headers.Add(HttpRequestHeader.Cookie, "connect.sid=" + box_cookie.Text);
+            string fn_string_clean ="";
+            string dl_dir = @"download\";
+            string pathstring = @"download\";
+            string albumstring = "";
+            pbar_updater(dq.queue.Count, 0);
+            string completestring;
+            Regex illegalInFileName = new Regex(@"[\\/:*?""<>|]");
+            for (int i = 0; i < dq.queue.Count -1; i++)
+            {
+                albumstring = dq.queue[i].getan();
+                pathstring = Path.Combine(Path.Combine(dl_dir,dq.queue[i].getartist()), albumstring);
+                fn_string_clean = illegalInFileName.Replace(dq.queue[i].getfn(), "-");
+                writeDebugFile("cleaned string:  " + fn_string_clean);
+                completestring = Path.Combine(Path.Combine(prgPath, pathstring), fn_string_clean) +"."+extension;
+                System.IO.Directory.CreateDirectory(Path.Combine(prgPath,pathstring));
+                writeDebugFile("queueDownloader: in for loop url: " + dq.queue[i].geturl() + "    | "+ "filename : " + completestring);
+                pbar_updater(dq.queue.Count, i);
+
+                if (File.Exists(completestring))
+                {
+                    writeDebugFile("skipping URL: " + dq.queue[i].geturl() + " file already exists");
+                }
+                else
+                {
+                 try
+                    {
+                        
+                        writeDebugFile("queueDownloader: downloading to: " + completestring);
+                        try { update_dl_title(dq.queue[i].getfn()); } catch { }
+                        using (WebClient fdwc= new WebClient())
+                        {
+                            fdwc.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+                            fdwc.Headers.Add(HttpRequestHeader.Cookie, "connect.sid=" + box_cookie.Text);
+                            fdwc.DownloadFile(dq.queue[i].geturl(), completestring);
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        writeDebugFile(e.ToString());
+                    }
+                }
+
+            }
+        }
+
+        private void chck_podcast_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chck_podcast.Checked) { dl_podcast = true; }
+        }
+
+
+
+
+        public class DownloadObject
+        {
+            public DownloadObject(string _url, string _filename, string _artist, string _albumname)
+            {
+                download_url = _url;
+                filename = _filename;
+                artist = _artist;
+                albumname = _albumname;
+            }
+            
+            public string geturl()
+            {
+                return download_url;
+
+            }
+            public string getfn()
+            {
+                return filename;
+            }
+            public string getartist()
+            {
+                return artist;
+            }
+            public string getan()
+            {
+                return albumname;
+            }
+            public void seturl(string url)
+            {
+                download_url = url;
+            }
+            string artist;
+            string download_url;
+            string filename;
+            string albumname;
+
+        }
+        public class DownloadQueue
+        {
+            public DownloadQueue()
+            {
+                queue = new List<DownloadObject>();
+            }
+            public void appendToQueue(DownloadObject d)
+            {
+
+                queue.Add(d);
+            }
+            public List<DownloadObject> queue;
+
+        }
+        public class Albums
+        {
+            public int trackNumber { get; set; }
+            public string albumId { get; set; }
+            public string _id { get; set; }
+            public bool isFree { get; set; }
+            public string streamHash { get; set; }
+        }
+        public class Artist
+        {
+            public string artistId { get; set; }
+            public string name { get; set; }
+            public string _id { get; set; }
+        }
+        public class Release
+        {
+            public string _id { get; set; }
+            public string title { get; set; }
+            public string renderedArtists { get; set; }
+            public string type { get; set; }
+            public string upc { get; set; }
+            public string catalogId { get; set; }
+            public string releaseDate { get; set; }
+            public object preReleaseDate { get; set; }
+            public string label { get; set; }
+            public bool deleted { get; set; }
+            public List<object> tags { get; set; }
+            public bool freeDownloadForUsers { get; set; }
+            public bool showToAdminsOnly { get; set; }
+            public bool showOnWebsite { get; set; }
+            public bool showAsFree { get; set; }
+            public List<string> urls { get; set; }
+            public string coverUrl { get; set; }
+        }
+        public class Result
+        {
+            public string _id { get; set; }
+            public string title { get; set; }
+            public string artistsTitle { get; set; }
+            public string created { get; set; }
+            public string label { get; set; }
+            public bool deleted { get; set; }
+            public bool hasErrors { get; set; }
+            public List<string> catalogs { get; set; }
+            public List<object> tags { get; set; }
+            public bool licensable { get; set; }
+            public List<object> genres { get; set; }
+            public Albums albums { get; set; }
+            public List<object> remixers { get; set; }
+            public List<object> featuring { get; set; }
+            public List<Artist> artists { get; set; }
+            public string isrc { get; set; }
+            public float? bpm { get; set; }
+            public double duration { get; set; }
+            public Release release { get; set; }
+            public bool streamable { get; set; }
+            public bool downloadable { get; set; }
+            public bool inEarlyAccess { get; set; }
+            public bool freeDownloadForUsers { get; set; }
+        }
+        public class MCatJSON
+        {
+
+            public int total { get; set; }
+            public int skip { get; set; }
+            public int limit { get; set; }
+            public List<Result> results { get; set; }
+        }
+
+        private void chck_mixes_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chck_mixes.Checked) { dl_mixes = true; }
+        }
+
+        private void box_cookie_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
